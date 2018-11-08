7b4b2a1aa0d30a6d1838265f9558d52f046909dc
DanielK <daniel.krupitsky@fyber.com>
2018-11-08 14:10:41 +0200

    1

--- src/main/scala/com/dk/Permutations.scala
+++ src/main/scala/com/dk/Permutations.scala
@@ -1,13 +1,40 @@
 package com.dk
 
 object Permutations extends App {
+
+  object Ugly {
+
+    def getAllPermutation(words:Seq[String], length:Int):Option[scala.collection.mutable.ArrayBuffer[Seq[String]]] = {
+      def printWordsPermutationRec(res:scala.collection.mutable.ArrayBuffer[Seq[String]],tempRes:Seq[String], words:Seq[String], length:Int):scala.collection.mutable.ArrayBuffer[Seq[String]] = {
+        length match{
+          case 0 => res += tempRes
+          case _ => for (word <- words){
+            val t = tempRes :+ word
+            printWordsPermutationRec(res,t, words.filter(_!=word), length-1)
+          }
+        }
+        res
+      }
+
+      if(words.lengthCompare(length) < 0 || length < 0)
+        None
+      else {
+        val s = scala.collection.mutable.ArrayBuffer[Seq[String]]()
+        val f =  Seq[String]()
+        Some(printWordsPermutationRec(s,f, words, length))
+      }
+    }
+
+
+  }
+
   def unsafePerms[A](seq: Seq[A], size: Int): Seq[Seq[A]] = {
     val seqSize = seq.size
 
     def loop(seq: Seq[A], size: Int): Seq[Seq[A]] = {
       def allInserts(x: A, ll: Seq[Seq[A]]): Seq[Seq[A]] = ll match {
-        case hd :: tl => inserts(x, hd) ++: allInserts(x, tl)
-        case _ => Nil
+        case hd :: tl ⇒ inserts(x, hd) ++: allInserts(x, tl)
+        case _ ⇒ Nil
       }
 
       def inserts(x: A, ll: Seq[A]): Seq[Seq[A]] =
@@ -33,11 +60,11 @@ object Permutations extends App {
 
     def loop(seq: Seq[A], size: Int): Trampoline[Seq[Seq[A]]] = {
       def allInserts(x: A, ll: Seq[Seq[A]]): Trampoline[Seq[Seq[A]]] = ll match {
-        case hd :: tl =>
+        case hd :: tl ⇒
           Suspend(() ⇒ allInserts(x, tl))
             .flatMap(all ⇒ Return(inserts(x, hd) ++: all))
 
-        case _ => Return(Nil)
+        case _ ⇒ Return(Nil)
       }
 
       def inserts(x: A, ll: Seq[A]): Seq[Seq[A]] =
@@ -67,33 +94,35 @@ object Permutations extends App {
     print(s.size + " , ")
     val y = time(perms(seq, i))
     print(y.size + " , ")
-    val x = time(unsafePerms(seq, i))
-    println(x.size)
+    val u = time(Ugly.getAllPermutation(seq.map(_.toString), i).toList.flatten)
+    print(u.size + " , ")
+    val x = time(s/*unsafePerms(seq, i)*/)
+    print(x.size + " , ")
 
+    println()
 
-    val xSet = x.toSet.toList
-    val ySet = y.toSet.toList
-    val sSet = s.toSet.toList
-
-    if (sSet.size != xSet.size || xSet.size != ySet.size || ySet != sSet)
-      println(s"${sSet.size} - ${xSet.size} - ${ySet.size}")
+    if (y.size != x.size || x.size != y.size || y.size != s.size || s.size != u.size)
+      println(s"${s.size} - ${x.size} - ${y.size} - ${u.size}")
 
     val check1 = sSet.zip(ySet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
     val check2 = sSet.zip(xSet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
-    if (check1.nonEmpty || check2.nonEmpty) println(check1 + "\n----\n" +check2)
+    val check3 = sSet.zip(xSet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
+    if (check1.nonEmpty || check2.nonEmpty || check3.nonEmpty) println(check1 + " - " + check2 + " - " + check3)
   }
 
   def time[A](block: ⇒ A): A = {
-    val pre = System.currentTimeMillis()
+    val pre = System.nanoTime()
     val b = block
-    val post = System.currentTimeMillis()
+    val post = System.nanoTime()
     print(s"${post - pre},")
 
     b
   }
 
+ // println(Ugly.getAllPermutation((1 to 8).toList.map(_.toString),3))
+
   for {
-    s <- 1 to 8
+    s <- 1 to 15
     j <- 1 to s
   } runAll((1 to s).toList, j)
 }
--- src/main/scala/com/dk/Trampoline.scala
+++ src/main/scala/com/dk/Trampoline.scala
@@ -1,23 +1,23 @@
 package com.dk
 
 sealed trait Trampoline[A] {
-  def flatMap[B](f: A => Trampoline[B]): Trampoline[B] = FlatMap(this, f)
+  def flatMap[B](f: A ⇒ Trampoline[B]): Trampoline[B] = FlatMap(this, f)
 }
 
 object Trampoline {
   def run[A](tr: Trampoline[A]): A = tr match {
-    case Return(a) => a
-    case Suspend(s) => run(s())
-    case FlatMap(t, f) => t match {
-      case Return(a) => run(f(a))
-      case Suspend(s) => run(s().flatMap(f))
-      case FlatMap(innerT, g) => run(innerT.flatMap(x ⇒ g(x) flatMap f))
+    case Return(a) ⇒ a
+    case Suspend(s) ⇒ run(s())
+    case FlatMap(t, f) ⇒ t match {
+      case Return(a) ⇒ run(f(a))
+      case Suspend(s) ⇒ run(s().flatMap(f))
+      case FlatMap(innerT, g) ⇒ run(innerT.flatMap(x ⇒ g(x) flatMap f))
     }
   }
 }
 
 final case class Return[A](a: A) extends Trampoline[A]
 
-final case class Suspend[A](s: () => Trampoline[A]) extends Trampoline[A]
+final case class Suspend[A](s: () ⇒ Trampoline[A]) extends Trampoline[A]
 
-final case class FlatMap[A, B](sub: Trampoline[A], f: A => Trampoline[B]) extends Trampoline[B]
+final case class FlatMap[A, B](sub: Trampoline[A], f: A ⇒ Trampoline[B]) extends Trampoline[B]
