c19b13d12520e930b2091ae60d81162de8e2707c
DanielK <daniel.krupitsky@fyber.com>
2018-11-09 17:34:22 +0200

    changes

--- src/main/scala/com/dk/Permutations.scala
+++ src/main/scala/com/dk/Permutations.scala
@@ -4,28 +4,26 @@ object Permutations extends App {
 
   object Ugly {
 
-    def getAllPermutation(words:Seq[String], length:Int):Option[scala.collection.mutable.ArrayBuffer[Seq[String]]] = {
-      def printWordsPermutationRec(res:scala.collection.mutable.ArrayBuffer[Seq[String]],tempRes:Seq[String], words:Seq[String], length:Int):scala.collection.mutable.ArrayBuffer[Seq[String]] = {
-        length match{
+    def getAllPermutation(words: Seq[String], length: Int): Option[scala.collection.mutable.ArrayBuffer[Seq[String]]] = {
+      def printWordsPermutationRec(res: scala.collection.mutable.ArrayBuffer[Seq[String]], tempRes: Seq[String], words: Seq[String], length: Int): scala.collection.mutable.ArrayBuffer[Seq[String]] = {
+        length match {
           case 0 => res += tempRes
-          case _ => for (word <- words){
+          case _ => for (word <- words) {
             val t = tempRes :+ word
-            printWordsPermutationRec(res,t, words.filter(_!=word), length-1)
+            printWordsPermutationRec(res, t, words.filter(_ != word), length - 1)
           }
         }
         res
       }
 
-      if(words.lengthCompare(length) < 0 || length < 0)
+      if (words.lengthCompare(length) < 0 || length < 0)
         None
       else {
         val s = scala.collection.mutable.ArrayBuffer[Seq[String]]()
-        val f =  Seq[String]()
-        Some(printWordsPermutationRec(s,f, words, length))
+        val f = Seq[String]()
+        Some(printWordsPermutationRec(s, f, words, length))
       }
     }
-
-
   }
 
   def unsafePerms[A](seq: Seq[A], size: Int): Seq[Seq[A]] = {
@@ -89,41 +87,51 @@ object Permutations extends App {
 
   def simple[A](seq: Seq[A], size: Int): Seq[Seq[A]] = seq.combinations(size).flatMap(_.permutations).toList
 
-  private def runAll[A](seq: Seq[A], i: Int) = {
-    val s = time(simple(seq, i))
-    print(s.size + " , ")
-    val y = time(perms(seq, i))
-    print(y.size + " , ")
-    val u = time(Ugly.getAllPermutation(seq.map(_.toString), i).toList.flatten)
-    print(u.size + " , ")
-    val x = time(s/*unsafePerms(seq, i)*/)
-    print(x.size + " , ")
+  private def runAll[A](seq: Seq[A], i: Int)(ops: ((Seq[A], Int) ⇒ Seq[Seq[A]])*): Unit = {
+    val res = ops map { op ⇒ time(op(seq, i)) }
 
-    println()
+    val meta = res map { case (t, s) ⇒ t -> s.size }
 
-    if (y.size != x.size || x.size != y.size || y.size != s.size || s.size != u.size)
-      println(s"${s.size} - ${x.size} - ${y.size} - ${u.size}")
+    println(meta.map(a ⇒ s"${a._1}, ${a._2}").mkString(" , "))
 
-    val check1 = sSet.zip(ySet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
-    val check2 = sSet.zip(xSet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
-    val check3 = sSet.zip(xSet).filterNot(e ⇒ e._1 == e._2).mkString("\n")
-    if (check1.nonEmpty || check2.nonEmpty || check3.nonEmpty) println(check1 + " - " + check2 + " - " + check3)
+    compareSeq(meta)((a, c) ⇒ c._2 == a._2).withFilter(!_._1).foreach(println(_))
+
+    compareSeq(res) { (a, c) ⇒
+      a._2.forall(x => c._2.contains(x))
+      c._2.forall(x => a._2.contains(x))
+    } withFilter (!_._1) foreach (println(_))
+
   }
 
-  def time[A](block: ⇒ A): A = {
+
+  def compareSeq[B](seq: Seq[B])(pred: (B, B) ⇒ Boolean) = {
+
+    seq.headOption.map {
+      h ⇒
+        seq.foldLeft((true, h)) {
+          (acc, curr) ⇒ if (acc._1) pred(curr, acc._2) -> curr else false -> acc._2
+        }
+    }
+  }
+
+  def time[A](block: ⇒ A): (Long, A) = {
     val pre = System.nanoTime()
     val b = block
     val post = System.nanoTime()
-    print(s"${post - pre},")
 
-    b
+    (post - pre, b)
   }
 
- // println(Ugly.getAllPermutation((1 to 8).toList.map(_.toString),3))
+  // println(Ugly.getAllPermutation((1 to 8).toList.map(_.toString),3))
 
   for {
     s <- 1 to 15
     j <- 1 to s
-  } runAll((1 to s).toList, j)
+  } runAll((1 to s).toList, j)(
+    simple,
+    perms,
+    (s, i) ⇒ Ugly.getAllPermutation(s.map(_.toString), i).toList.flatten.map(_.map(_.toInt)),
+    simple //unsafePerms(seq, i)
+  )
 }
 
